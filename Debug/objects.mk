23d938fcd2e991de77947409960655313115c396
Doug_Shin <dgsh300@gmail.com>
2025-04-22 14:39:52 +0900

    적용: .gitignore 파일 추가 및 불필요한 파일 추적 중단

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lIQmath_fpu32_coff.lib -lrts2800_fpu32_fast_supplement_coff.lib -lrts2800_fpu32.lib -llibc.a
-
